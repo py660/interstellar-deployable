e72bd88937742e41fa45d8176bef2f14c0d240cc
py660 <93660666+py660@users.noreply.github.com>
2023-05-24 19:02:24 -0400

    Update main.js

--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -6,13 +6,13 @@ if (gaenabled == "false") {
   gascript.setAttribute("async", "");
   gascript.setAttribute(
     "src",
-    "https://www.googletagmanager.com/gtag/js?id=G-WKJQ5QHQTJ"
+    "https://www.googletagmanager.com/gtag/js?id=G-LFM92GJKJZ"
   );
   const inlinegascript = document.createElement("script");
   inlinegascript.innerHTML = `window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
-        gtag('config', 'G-WKJQ5QHQTJ');`;
+        gtag('config', 'G-LFM92GJKJZ');`;
   document.head.append(gascript, inlinegascript);
   console.log("Injected script 1/2");
 }
